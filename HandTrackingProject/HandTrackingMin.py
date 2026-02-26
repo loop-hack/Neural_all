5dbf4f696f9d118a07cddf8468595059f543d9fc
loop-hack <timetraveller1732@gmail.com>
2026-02-26 19:12:53 +0530

    bug fix

--- HandTrackingProject/HandTrackingMin.py
+++ HandTrackingProject/HandTrackingMin.py
@@ -86,10 +86,6 @@ while True:
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
 
-    # -------------------------
-    # SHOW OUTPUT
-    # -------------------------
-
     cv2.imshow("Hand + Face Tracking", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
